2c0ebc198d1ad98274d7a02799c9a429aa82b6b0
Mariona Nadal <70766382+DSRschool@users.noreply.github.com>
2022-08-18 10:37:05 +0000

    Correcciones de formato

--- src/es/dsrroma/school/java/desafio1/video01_09/EdadExacta.java
+++ src/es/dsrroma/school/java/desafio1/video01_09/EdadExacta.java
@@ -12,30 +12,35 @@ public class EdadExacta {
 
 	public static void main(String[] args) {
 		try (Scanner s = new Scanner(System.in)) {
-			System.out.println("Indica tu fecha de nacimiento (" + PATRON + ")");
+			System.out.println(
+				"Indica tu fecha de nacimiento (" + PATRON + ")");
 			String fechaLeida = s.nextLine();
 			try {
 				DateTimeFormatter dtf = DateTimeFormatter.ofPattern(PATRON);
 				LocalDate fecha = LocalDate.parse(fechaLeida, dtf);
 				LocalDate ahora = LocalDate.now();
 				Period edad = Period.between(fecha, ahora);
-				System.out.println(String.format("Edad: %d años, %d meses y %d días",
+				System.out.println(
+					String.format("Edad: %d años, %d meses y %d días",
 						edad.getYears(), edad.getMonths(), edad.getDays()));
 				if (edad.getMonths() == 0) {
 					if (edad.getDays() == 0) {
-						System.out.println("¡FELICIDADES! ¡Hoy es tu cumpleaños!");
+						System.out.println(
+							"¡FELICIDADES! ¡Hoy es tu cumpleaños!");
 					} else {
-						System.out.println("¡Felicidades! Tu cumpleaños ha sido hace poco...");						
+						System.out.println(
+							"¡Felicidades! Tu cumpleaños ha sido hace poco...");						
 					}
 				} else if (edad.getMonths() == 11) {
-					System.out.println("¡Felicidades! Tu cumpleaños se acerca...");						
+					System.out.println(
+						"¡Felicidades! Tu cumpleaños se acerca...");						
 				}
 			} catch (DateTimeParseException e) {
 				System.err.println(String.format(
-						"La fecha indicada ('%s') no sigue el patrón esperado ('%s').", 
+					"La fecha indicada ('%s') " 
+					+ "no sigue el patrón esperado ('%s').", 
 						fechaLeida, PATRON)); 
 			}
 		}
 	}
-
 }
